c5f0d6d043f8ea24c01da9751e0c4a4abd14c0b3
aleksei <aplay675@gmail.com>
2023-12-04 12:29:38 +0300

    docker

--- blog/blog_posts/signals.py
+++ blog/blog_posts/signals.py
@@ -25,17 +25,17 @@ def log_deleted_object(sender, instance, **kwargs):
 #         from_email = settings.EMAIL_HOST_USER
 #         to_email = instance.user.email
 #         send_mail(subject, message, from_email, [to_email])
-@receiver(post_save, sender=Post)
-def post_save_send_email_notification(sender, instance, created, **kwargs):
-    if created:
-        send_email_notification.delay(instance.id)
+# @receiver(post_save, sender=Post)
+# def post_save_send_email_notification(sender, instance, created, **kwargs):
+#     if created:
+#         send_email_notification.delay(instance.id)
 
 
-@receiver(post_save, sender=Post)
-def create_comment_on_post_creation(sender, instance, created, **kwargs):
-    if created:
-        Comment.objects.create(
-            post=instance,
-            content="Please follow the rules of conduct in comments.",
-            author=Author.objects.get(username='system'),
-        )
+# @receiver(post_save, sender=Post)
+# def create_comment_on_post_creation(sender, instance, created, **kwargs):
+#     if created:
+#         Comment.objects.create(
+#             post=instance,
+#             content="Please follow the rules of conduct in comments.",
+#             author=Author.objects.get(username='system'),
+#         )
